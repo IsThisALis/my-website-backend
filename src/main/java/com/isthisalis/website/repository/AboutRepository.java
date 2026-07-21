c21f7874dd66c52053dadb3160e7c8ecab6c2d13
IsThisALis <gamemodebronze2@gmail.com>
2026-07-21 13:47:45 +0500

    fix(repository) - add CrossOrigin in repositories

--- src/main/java/com/isthisalis/website/repository/AboutRepository.java
+++ src/main/java/com/isthisalis/website/repository/AboutRepository.java
@@ -2,11 +2,12 @@ package com.isthisalis.website.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.isthisalis.website.entity.About;
 
 /**
  * AboutRepository
  */
-  @Repository
+  @Repository @CrossOrigin(origins = "https://isthisalis.github.io")
 public interface AboutRepository extends JpaRepository<About, Integer> {}
--- src/main/java/com/isthisalis/website/repository/ProjectRepository.java
+++ src/main/java/com/isthisalis/website/repository/ProjectRepository.java
@@ -2,11 +2,12 @@ package com.isthisalis.website.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.isthisalis.website.entity.Project;
 
 /**
  * ProjectRepository
  */
-  @Repository
+  @Repository @CrossOrigin(origins = "https://isthisalis.github.io")
 public interface ProjectRepository extends JpaRepository<Project, Integer> {}
