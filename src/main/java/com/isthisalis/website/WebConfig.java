bb089f2f4f39c4d490bae4616e8c4d9235ee3713
IsThisALis <gamemodebronze2@gmail.com>
2026-07-23 14:26:04 +0500

    fix(WebConfig) - enable WebMvc

--- src/main/java/com/isthisalis/website/WebConfig.java
+++ src/main/java/com/isthisalis/website/WebConfig.java
@@ -2,12 +2,13 @@ package com.isthisalis.website;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * WebConfig
  */
-  @Configuration
+  @Configuration @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
