3eadd1e5ef0b76d27f2eac72fad72457ce33cf37
IsThisALis <gamemodebronze2@gmail.com>
2026-07-23 15:09:08 +0500

    fix(CORS) - fix SecurityConfig

--- src/main/java/com/isthisalis/website/SecurityConfig.java
+++ src/main/java/com/isthisalis/website/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.isthisalis.website;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
   @Configuration @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
   public SecurityFilterChain configure(HttpSecurity httpsec) {
     httpsec.cors(Customizer.withDefaults())
       .csrf(csrf -> csrf.disable())
