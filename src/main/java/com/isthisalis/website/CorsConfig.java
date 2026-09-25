b7239511edf52d22fb60a9eb8787ff9d565678a3
IsThisALis <gamemodebronze2@gmail.com>
2026-09-25 23:27:28 +0500

    chore: change allowed HTTP methods

--- src/main/java/com/isthisalis/website/CorsConfig.java
+++ src/main/java/com/isthisalis/website/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of("https://isthisalis.github.io"));
-    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "HEAD"));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
     configuration.setAllowCredentials(true);
     configuration.setMaxAge(3600L);
