14d0d0da9bf5a1feed7b4fc86c3f3f3c09ae5146
IsThisALis <gamemodebronze2@gmail.com>
2026-09-26 17:08:01 +0500

    fix: change endpoints in security config

--- src/main/java/com/isthisalis/website/SecurityConfig.java
+++ src/main/java/com/isthisalis/website/SecurityConfig.java
@@ -17,13 +17,14 @@ public class SecurityConfig {
 
     @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpsec) {
+    String api = "/api";
     httpsec.cors(Customizer.withDefaults())
       .csrf(csrf -> csrf.disable())
       .sessionManagement(session -> { session.sessionCreationPolicy(SessionCreationPolicy.STATELESS); } );
     httpsec.authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST, "/comments").permitAll()
-                .requestMatchers(HttpMethod.GET, "/posts", "/abouts", "/projects", "/comments").permitAll()
-                .requestMatchers(HttpMethod.GET, "/posts/**", "/abouts/**", "/projects/**", "/comments/**").permitAll()
+                .requestMatchers(HttpMethod.POST, api + "/comments").permitAll()
+                .requestMatchers(HttpMethod.GET, api + "/posts", api + "/abouts", api + "/projects", api + "/comments").permitAll()
+                .requestMatchers(HttpMethod.GET, api + "/posts/**", api + "/abouts/**", api + "/projects/**", api + "/comments/**").permitAll()
                 .requestMatchers("/**").authenticated()
                 .anyRequest().authenticated());
                 httpsec.httpBasic(Customizer.withDefaults());
