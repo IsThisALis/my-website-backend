f73139e63eee355e533a8e112412495f22268f83
IsThisALis <gamemodebronze2@gmail.com>
2026-09-26 17:26:24 +0500

    feat: add heathcheck endpoint

---
+++ src/main/java/com/isthisalis/website/controller/ApiController.java
@@ -0,0 +1,21 @@
+package com.isthisalis.website.controller;
+
+import java.time.Instant;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    
+    @GetMapping("/health")
+    public Map<String, String> checkHealth() {
+        return Map.of(
+            "status", "UP",
+            "timestamp", Instant.now().toString()
+        );
+    }
+}
